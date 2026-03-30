68f4b80ed5ae9c6dcb0a34ed7a63c925cd8f44f5
Teafed <xjakea45x@yahoo.com>
2026-03-30 15:14:59 -0700

    missed env_config rename oops

--- callbacks.py
+++ callbacks.py
@@ -200,8 +200,8 @@ class CurriculumCallback(BaseCallback):
                self._save_path,
                f"stage_{prev_stage_idx:02d}_checkpoint")
             self.model.save(ckpt)
-            from train import _save_training_config
-            _save_training_config(self._save_path, self.curriculum, self.num_timesteps)
+            from train import _save_env_config
+            _save_env_config(self._save_path, self.curriculum, self.num_timesteps)
             if self.verbose >= 1:
                print(f"[curriculum] checkpoint saved: {ckpt}.zip; training config updated")
          # log new stage index so tensorboard shows the transition
